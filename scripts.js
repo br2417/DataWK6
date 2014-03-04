23a03dcf4d2d4a1d165a0c105e9ad28ef9b50883
Benedetta Riva <benedettariva@dyn-160-39-171-242.dyn.columbia.edu>
2014-03-04 17:10:02 -0500

    first draft wk6 assign / changes made to scripts file

--- scripts.js
+++ scripts.js
@@ -2,90 +2,67 @@
  * @author
  */
 
-	console.log("hi there"); 
+	/*  OUTLINE: 
+	1. Get data from FRED
+	2. Create Google fusion table with data from FRED (or other data source)
+	3. Pull data back from fusion table using SQL (querying for data) - add fusion table ID (and personal Google Fusion API key) to URL
+	4. Use new URL as data source (no longer using static JSON file)
+	5. Update code to draw chart 
+	6. Change range of data used by chart by updating the URL using SQL 
 	
-	//UNEMPDATA is the local name of the json file I just loaded
+	*/
+	console.log("hello there"); 
 	
-	function dataLoaded(UNEMPDATA){
+	//CivUnempRate is the local name of the json file that will be loaded
+	
+	function mDataLoaded(CivUnempRate){
 		
-		console.log(UNEMPDATA)
+		console.log(CivUnempRate)
 		
-		var gDataTable = new google.visualization.DataTable();
+		//The below variable feeds the data to visualization library
+		var gUnempTable = new google.visualization.DataTable();
 		
-		//wehn I add columns (column names) the first parameter is thr data type in that column
+		//when I add columns (column names) the first parameter is the data type in that column
 		//the 2nd paramenter is the name of the column
 		
+		gUnempTable.addColumn('string', CivUnempRate.columns[0]);  //only works because this is a google.visualization
+		gUnempTable.addColumn('number', CivUnempRate.columns[1]);
 		
-		gDataTable.addColumn('string', UNEMPDATA.columns[0]);  //only works because this is a google.visualization
-		gDataTable.addColumn('number', UNEMPDATA.columns[1]);
-		
-		gDataTable.addRows(UNEMPDATA.rows) //only works because this is a google.visualization
-		
+		gUnempTable.addRows(CivUnempRate.rows) //only works because this is a google.visualization
 		
 		
-		/*
-		var myObsData = UNEMPDATA.observations; 
-		
-		///i am trying to construct an array of arrays
-		
-		var myDataArray = [];
-		
-		//?console log my DataArray
-		
-		//wait a second, I need headers in the first row
-		//create a header array and push to 
-		
-		
-		var headerArray = ["Date", "Value"];
-		myDataArray.push(headerArray);
-		
-		
-		
-		//push always puts things onto the end of the array
-		
-		// specify starting point, ending point, 
-		for(var i=0; i<myObsData.length; i++){
-			
-			//create reference to current object in list
-			var currObj = myObsData[i];
-			
-			//below is the Number function, basically says 'treat this as a number.' If you need to make it a string, you would write String.
-			var currArray = [currObj.date, Number(currObj.value)];
-			
-			//myDataArray is the array of arrays
-			
-			myDataArray.push(currArray);
-			
-		} //end of for loop
-		
-		console.log(myDataArray);
-		
-		//fed data to visualization library
-		 var myDataTable = google.visualization.arrayToDataTable(myDataArray);
+		//With Google Visualizations we have the option to customize our charts
+		//Below, I used the gChartOptions variable to create an object to add a title to the chart
+		// I also used the gChartOptions variable to create objects to adjust the size of the chart, and its position from the left border
 		 
-		 
-		 //create options object to actually customize the look of the chart
-		 
-		 */
-		 
-		 var chartOptions = {
-          title: "Unemployment since 1948"
+		 var gChartOptions = {
+         title: "Civil Unemployment Rate since 1948",     
+    	 width: 1500,
+       	 height: 500,
+       	 chartArea: {
+       	 	left: 100
+      	 }
+        
         };
 
-
-		//tell it to create a line chart
-		var myChart = new google.visualization.LineChart(document.getElementById("myChartDiv"));
-  			myChart.draw(gDataTable, chartOptions);
+	
+		//Below we tell the computer to create a line chart
+		//We use a function that is part of the Google Visualizations library 
+		
+		var gUnempChart = new google.visualization.LineChart(document.getElementById("myChartDiv"));
+  			gUnempChart.draw(gUnempTable, gChartOptions);
    	
 	}
 	
-	
-	function googleLoaded(){
+		//Below I have created and named the function gloaded
+
+	function gLoaded(){
 		
 		console.log("google loaded");
 		
-		//  * instead of loading data from a static JSON file I'm going to laotd it from a google fusion table
-		$.get("https://www.googleapis.com/fusiontables/v1/query?sql=SELECT+*+FROM+12A0eKUXKb4TCFhngfaGqaMEH6uECY1iBhd3VOBAV&key=AIzaSyDsBrSpJRliKgi913vr9FWTy8oL57c42bA", dataLoaded, "json");
+		// instead of loading data from a static JSON file I'm going to laod it from a google fusion table
+		// we do this by using the URL with our fusion table ID
+		$.get("https://www.googleapis.com/fusiontables/v1/query?sql=SELECT+*+FROM+12A0eKUXKb4TCFhngfaGqaMEH6uECY1iBhd3VOBAV&key=AIzaSyDsBrSpJRliKgi913vr9FWTy8oL57c42bA", mDataLoaded, "json");
 		
 		
 		// in order to have two charts on the same page we would need a second get request. 
@@ -94,17 +71,19 @@
 		//we would also need a new div
 	}
 		
-	
-	function pageLoaded(){
+		//Below I have created and named the function mPageLoaded
+		
+	function mPageLoaded(){
 		
 		console.log("got to page Loaded");
 		
 		//load the google visualization library 
-		//added the callback - want the name of the callback function to be googleLoaded
+		//added the callback - want the name of the callback function to be gLoaded
 		
-		google.load("visualization", "1", {packages:["corechart"], callback: "googleLoaded"});
+		google.load("visualization", "1", {packages:["corechart"], callback: "gLoaded"});
 
 		
 	}
-	
-	$(document).ready(pageLoaded);
+		//The below is a jQuery function that makes sure that the document is ready before loading the data on the page
+		//The name of the call-back is BennysPageLoaded
+	$(document).ready(mPageLoaded);
