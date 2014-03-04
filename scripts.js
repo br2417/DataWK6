32abda89bb6691a80f1444dcbc3487e4b32db396
Benedetta Riva <benedettariva@dyn-160-39-171-242.dyn.columbia.edu>
2014-03-04 17:27:25 -0500

    refined URL query and customised size of table

--- scripts.js
+++ scripts.js
@@ -36,7 +36,7 @@
 		// I also used the gChartOptions variable to create objects to adjust the size of the chart, and its position from the left border
 		 
 		 var gChartOptions = {
-         title: "Civil Unemployment Rate since 1948",     
+         title: "Civil Unemployment Rate since 1990",     
     	 width: 1500,
        	 height: 500,
        	 chartArea: {
@@ -62,7 +62,10 @@
 		
 		// instead of loading data from a static JSON file I'm going to laod it from a google fusion table
 		// we do this by using the URL with our fusion table ID
-		$.get("https://www.googleapis.com/fusiontables/v1/query?sql=SELECT+*+FROM+12A0eKUXKb4TCFhngfaGqaMEH6uECY1iBhd3VOBAV&key=AIzaSyDsBrSpJRliKgi913vr9FWTy8oL57c42bA", mDataLoaded, "json");
+		// I have also changed the range of data that is charted by refining the query in the URL 
+		// I did this by adding the following instructions in SQL WHERE+DATE>'1989-12-01'
+		
+		$.get("https://www.googleapis.com/fusiontables/v1/query?sql=SELECT+*+FROM+12A0eKUXKb4TCFhngfaGqaMEH6uECY1iBhd3VOBAV+WHERE+DATE>'1989-12-01'&key=AIzaSyDsBrSpJRliKgi913vr9FWTy8oL57c42bA", mDataLoaded, "json");
 		
 		
 		// in order to have two charts on the same page we would need a second get request. 
